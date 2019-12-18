00568fd76094b73ddb795a4477bcba8930c726ab
Stickyamp11 <55752067+Stickyamp11@users.noreply.github.com>
2019-12-18 14:48:16 +0100

    Create tratamiento.h

---
+++ Prototipos/tratamiento.h
@@ -0,0 +1,25 @@
+#ifndef TRATAMIENTO_H
+#define TRATAMIENTO_H
+class Tratamiento {
+
+private:
+
+int codigo_;
+int cantidaddiaria_;
+int duracion_;
+string doctorquereceta_;
+
+public:
+	Tratamiento(int codigo,int cantidaddiaria,int duracion, string doctorquereceta);
+	inline int getCodigo()const {return codigo_;};
+	inline void setCodigo(int a){ codigo_=a;};
+	inline string getCantidaddiaria()const {return cantidaddiaria_;};
+	inline void setCantidaddiaria(string a){cantidaddiaria_=a;};
+	inline string getDuracion()const{return duracion_};
+	inline void setDuracion(string a){duracion_=a;};
+	inline string getDoctorquerecta()const { return doctorquereceta_;};
+	inline void setDoctorquereceta(string a){doctorquereceta_=a;};
+
+};
+
+#endif
