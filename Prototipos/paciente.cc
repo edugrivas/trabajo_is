146356ba2a8c8d24b55828f9c246ff754ae9e46d
Stickyamp11 <55752067+Stickyamp11@users.noreply.github.com>
2019-12-18 14:27:19 +0100

    Create paciente.cc

---
+++ Prototipos/paciente.cc
@@ -0,0 +1,12 @@
+#include "paciente.h"
+Paciente::Paciente(string nombre, string apellidos,string DNI, int idhistorial,string fechanacimiento,int CP, string telefono)
+{
+nombre_=nombre;
+apellidos_=apellidos;
+DNI_=DNI;
+idhistorial_=idhistorial;
+CP_=CP;
+fechanacimiento_=fechanacimiento;
+telefono_=telefono;
+}
+
