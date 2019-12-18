cada720f7979e65cebb5651b44ae1866db86b147
Stickyamp11 <55752067+Stickyamp11@users.noreply.github.com>
2019-12-18 13:57:00 +0100

    Update paciente.h

--- Prototipos/paciente.h
+++ Prototipos/paciente.h
@@ -18,13 +18,17 @@ public:
   inline string getNombre() const {return nombre_;};
   inline string getApellidos() const {return apellidos_;};
   inline string getFecha() const {return fechanacimiento_;};
-  inline string getTelefono() const {return telefono_;}
+  inline string getTelefono() const {return telefono_;};
+  inline string getDNI()const { return DNI_;};
   inline int getCP() const {return CP_;};
   inline int getIdhistorial() const {return idhistorial_;};
-  inline void setNombre(string a) {nombre_ = a;}
-  inline void setApellidos(string a) {apellidos_ = a;}
-  void setCP(int a);
-  void setFecha(string a);
-  void setTelefono(string a);
+  inline void setNombre(string a) {nombre_ = a;};
+  inline void setApellidos(string a) {apellidos_ = a;};
+  inline void setDNI(string a){DNI_=a;};
+  inline void setCP(int a){CP_=a;};
+  inline void setFecha(string a){fechanacimiento_=a;};
+  inline void setTelefono(string a){telefono_=a;};
+
 };
-#endif;
+
+#endif
