5865a6bbe4232911208426f127e8dc62aae7d0de
Stickyamp11 <55752067+Stickyamp11@users.noreply.github.com>
2019-12-18 14:48:51 +0100

    Create tratamiento.cc

---
+++ Prototipos/tratamiento.cc
@@ -0,0 +1,7 @@
+#include "tratamiento.h"
+Tratamiento::Tratamiento(int codigo,int cantidaddiaria,int duracion, string doctorquereceta){
+codigo_=codigo;
+cantidaddiaria_=cantidaddiaria;
+duracion_=duracion;
+doctorquereceta_=doctorquereceta;
+}
