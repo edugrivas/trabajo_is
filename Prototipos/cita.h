d7bac6b0426d6748973dd69ca2abb2c50c9c74fc
Stickyamp11 <55752067+Stickyamp11@users.noreply.github.com>
2019-12-19 13:06:05 +0100

    Update cita.h

--- Prototipos/cita.h
+++ Prototipos/cita.h
@@ -1,25 +1,29 @@
 #ifndef CITA_H
 #define CITA_H
-#include <string>
+#include <cstring>
 #include <cstdio>
 #include <cstdlib>
-
+#include <iostream>
+using namespace std;
 class Cita {
 private:
-int codigo_;
+string codigo_;
 string fecha_;
 string hora_;
 string doctor_;
+string dni_;
 
 public:
-	Cita(int codigo,string fecha,string hora);
-	inline int getCodigo()const {return codigo_;};
-	inline void setCodigo(int a){ codigo_=a;};
+	Cita(string codigo,string fecha,string hora,string dni,string doctor="");
+	inline string getCodigo()const {return codigo_;};
+	inline void setCodigo(string a){ codigo_=a;};
 	inline string getFecha()const {return fecha_;};
 	inline void setFecha(string a){fecha_=a;};
-	inline string getHora()const{return hora_};
+	inline string getHora()const{return hora_;};
 	inline void setHora(string a){hora_=a;};
 	inline string getDoctor()const { return doctor_;};
 	inline void setDoctor(string a){doctor_=a;};
+	inline string getDNI()const { return dni_;};
+	inline void setDNI(string a){dni_=a;};
 };
 #endif
