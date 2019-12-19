7ea142ab35eb65520a670044fc8a1ec1f8783e34
Stickyamp11 <55752067+Stickyamp11@users.noreply.github.com>
2019-12-19 13:05:41 +0100

    Update cita.cc

--- Prototipos/cita.cc
+++ Prototipos/cita.cc
@@ -1,7 +1,9 @@
 #include "cita.h"
-Cita::Cita(int codigo,string fecha,string hora)
+Cita::Cita(string codigo,string fecha,string hora,string dni,string doctor)
 {
 codigo_=codigo;
 fecha_=fecha;
 hora_=hora;
+dni_=dni;
+doctor_=doctor;
 }
