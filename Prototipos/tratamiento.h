2a74a073f48504e0860c1603cd5f92a18f83d95a
Stickyamp11 <55752067+Stickyamp11@users.noreply.github.com>
2019-12-19 13:06:44 +0100

    Update tratamiento.h

--- Prototipos/tratamiento.h
+++ Prototipos/tratamiento.h
@@ -1,25 +1,30 @@
 #ifndef TRATAMIENTO_H
 #define TRATAMIENTO_H
-#include <string>
+#include <cstring>
+#include <iostream>
+using namespace std;
 class Tratamiento {
 
 private:
 
-int codigo_;
+string dni_;
 int cantidaddiaria_;
-int duracion_;
+string duracion_;
 string doctorquereceta_;
+string medicamento_;
 
 public:
-	Tratamiento(int codigo,int cantidaddiaria,int duracion, string doctorquereceta);
-	inline int getCodigo()const {return codigo_;};
-	inline void setCodigo(int a){ codigo_=a;};
-	inline string getCantidaddiaria()const {return cantidaddiaria_;};
-	inline void setCantidaddiaria(string a){cantidaddiaria_=a;};
-	inline string getDuracion()const{return duracion_};
+	Tratamiento(string dni,int cantidaddiaria,string duracion, string doctorquereceta,string medicamento);
+	inline string getDNI()const {return dni_;};
+	inline void setDNI(string a){ dni_=a;};
+	inline int getCantidaddiaria()const {return cantidaddiaria_;};
+	inline void setCantidaddiaria(int a){cantidaddiaria_=a;};
+	inline string getDuracion()const{return duracion_;};
 	inline void setDuracion(string a){duracion_=a;};
 	inline string getDoctorquereceta()const { return doctorquereceta_;};
 	inline void setDoctorquereceta(string a){doctorquereceta_=a;};
+		inline string getMedicamento()const{return medicamento_;};
+	inline void setMedicamento(string a){medicamento_=a;};
 
 };
 
