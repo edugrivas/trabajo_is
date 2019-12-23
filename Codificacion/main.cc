891886fff79fc1802a74f4c5187f4684736f6376
Stickyamp11 <55752067+Stickyamp11@users.noreply.github.com>
2019-12-23 19:11:07 +0100

    Update main.cc

--- Codificacion/main.cc
+++ Codificacion/main.cc
@@ -10,90 +10,116 @@
 using namespace std;
 
 int main(){
-	ListaPaciente aux3;   //auxiliares
-	Historial constante;
-	string pausa;
-int x=0;
-string aux6;
+//INICIO DEL PROGRAMA
+ListaPaciente aux3;   //Objetos auxiliares necesarios para que funcione
+Historial constante;	
+int x=0; //El entero que nos mueve entre las opciones
+string aux6; 
 
 aux3.leePacientes();
-aux3.leeCitas();
-aux3.leeTratamientos();   //Cada que inicia, carga los ficheros de datos
+aux3.leeCitas(); 	 //Cada que inicia, carga los ficheros de datos
+aux3.leeTratamientos();  
+	
+	
 constante.leehistorialCitas();          //Historial independiente
 constante.leehistorialTratamientos();
-while(x!=6){
-	system("clear");
+	
+	
+while(x!=6){ //Menú en bucle   //Aclaración, uso llamadas system("clear") para limpiar pantalla y cin.get() para pausar. 
+	system("clear"); 
+	
+	
 cout<<"MENU INTERFAZ DE LA CLINICA"<<endl;
 cout<<endl;
 cout<<endl;
 cout<<"1.Anadir paciente a la base de datos"<<endl;
 cout<<"2.-Buscar paciente en la base de datos"<<endl;
-cout<<"3.-Consultar cita medica"<<endl;               //interfaz ciclica
+cout<<"3.-Consultar cita medica"<<endl;               //Interfaz que muestra la pantalla
 cout<<"4.-Seleccionar paciente"<<endl;
 cout<<"5.-Listado de todos los pacientes"<<endl;
 cout<<"6.-Salir del programa"<<endl;
 cout<<"7.-Mostrar citas diarias"<<endl;
 
 cin>>x;
-if(x==1){
+	
+if(x==1)
+{
 string nombre1,apellidos1,DNI,aux5; //variables add paciente
 string idhistorial;   //id paciente
 cin.clear();
-cin.ignore();
-cout<<"Introduce los apellidos del paciente"<<endl; //parametros obligatorios
+cin.ignore(); //Para que funcione el getline
+cout<<"Introduce los apellidos del paciente"<<endl;       //parametros obligatorios
 getline(cin,apellidos1);
 cout<<"Introduce el nombre del paciente"<<endl;
 cin>>nombre1;
 cout<<"Introduce el dni del paciente"<<endl;
 cin>>DNI;
-aux5=DNI.substr(3,2);
-idhistorial=aux5;  //un simple id
-if(1!=aux3.anadepaciente(nombre1,apellidos1,DNI,idhistorial)){cout<<"error"<<endl;}
-else {cout<<"Paciente anadido con exito"<<endl;}
+aux5=DNI.substr(3,2);  //Aquí hago una serie de transformaciones que crean un código id.
+idhistorial=aux5;
+	
+if(1!=aux3.anadepaciente(nombre1,apellidos1,DNI,idhistorial)){cout<<"error"<<endl;} //creo el paciente
+else { cout<<"Paciente anadido con exito"<<endl; }
+	
 cin.ignore();
 cin.get();
 }
 
 if(x==2)
-{system("clear");
-	string aux7;
-	int ayuda=0;
+{
+system("clear");
+string aux7;
+int ayuda=0;
+	
 cout<<"Indique el DNI del paciente a buscar"<<endl;
-cin>>aux7;     
+cin>>aux7; 
+	
 if(aux3.checkDNI(aux7)==1)  //Busco con el dni en la lista de pacientes
-{ system("clear");
-aux3.mostrarpaciente(aux7);    //muestro el paciente si lo encuentra
-cout<<"\n Desea modifica este paciente? 0/1: ";
-cin>>ayuda;
-cout<<"\n";  //Doy la opcion de ir al paciente
-if(ayuda==1){x=4; aux6=aux7;}  //Establezco valores para el paciente a modificar
-
-}
+	{ 
+	system("clear");
+	aux3.mostrarpaciente(aux7);    //muestro el paciente si lo encuentra
+	cout<<"\n Desea modifica este paciente? 0/1: ";
+	cin>>ayuda;    //Doy la opcion de ir al menú del paciente
+	cout<<"\n";
+	if(ayuda==1){x=4; aux6=aux7;}  //Establezco valores para el paciente a modificar
+	}
+	
 else cout<<"No encontrado\n"<<endl;
+	
 cin.ignore();
 cin.get();
 }
-if(x==3){
-	string aux8;
-	cout<<"Introduce el DNI del paciente de la cita"<<endl;
-	cin>>aux8;
-	aux3.mostrarcitasdepaciente(aux8);  //Muestro las citas del paciente
+	
+if(x==3)
+{
+string aux8;
+	
+cout<<"Introduce el DNI del paciente de la cita"<<endl;
+cin>>aux8;
+aux3.mostrarcitasdepaciente(aux8);  //Muestro las citas del paciente
+	
 cin.ignore();
 cin.get();
 }
-if(x==5){
+	
+if(x==5)
+{
 aux3.mostrarpacientes();  //Muestro todos los pacientes
 cout<<"\n\n\n";
+	
 cin.ignore();
 cin.get();
 
 
 }
+	
 if(x==7)
-{ string fecha1;
-	cout<<"Introduce la fecha asi: XX/XX/XXXX"<<endl;
-	cin>>fecha1;
-aux3.mostrarcitasdiarias(fecha1);
+{ 
+string fecha1;
+cout<<"Introduce la fecha asi: XX/XX/XXXX"<<endl; //Formato XX/XX/XXXX
+cin>>fecha1;
+	
+aux3.mostrarcitasdiarias(fecha1); //Muestra las citas que tienen esa fecha
+	
 cin.ignore();
 cin.get();
 }
@@ -104,15 +130,16 @@ cin.get();
 
 
 
-if(x==4){
+if(x==4)
+{
 int aux2=0;
 cout<<"Indique el DNI del paciente"<<endl;
 cin>>aux6;   //Guardo el dni
-if(aux3.checkDNI(aux6)==1)  //compruebo que existe en la base de datos
+if(aux3.checkDNI(aux6)==1)  //compruebo que existe en la base de datos ese dni
 								{
 
 
-while(aux2!=7)
+while(aux2!=7)   //Segundo Menú
 {
 	system("clear");
 cout<<"Panel de control del paciente :    "<<aux6<<endl;
@@ -121,14 +148,16 @@ cout<<endl;
 cout<<"1.-Anadir tratamiento"<<endl;
 cout<<"2.-Anadir cita medica"<<endl;
 cout<<"3.-Ver historial medico"<<endl;
-cout<<"4.-Ver citas medicas"<<endl;   //Interfaz de paciente concreto
+cout<<"4.-Ver citas medicas"<<endl;        //Interfaz de paciente concreto
 cout<<"5.-Ver tratamientos"<<endl;
 cout<<"0.-Modificar datos del paciente"<<endl;
 cout<<"6.-Eliminar paciente de la base de datos"<<endl;
 cout<<"7.-Volver atras"<<endl;
 cout<<endl;
 cout<<endl;
+	
 cin>>aux2;
+	
 if(aux2==6){
 	int error;
 	Paciente paciente("hola","hola",aux6,"hola");
@@ -154,17 +183,20 @@ cout<<"Introduce la hora"<<endl; //parametros obligatorios
 cin>>hora;
 cout<<"Introduce el doctor"<<endl;
 cin>>doctor;
+	
 codigo=aux6.substr(3,2)+fecha.substr(1,1)+doctor.substr(1,1);  //Esta es la "formula" para generar el codigo, algo basica.
 	Cita cita2(codigo,fecha,hora,aux6,doctor);
 	aux3.addCita(cita2);  //Añadimos a citas
 	constante.addCitahistorial(cita2);  //al historial
 	cout<<"Cita anadida\n"<<endl;
+	
 cin.ignore();
 cin.get();
 }
-if(aux2==1){
-	system("clear");
-	string duracion,doctorquereceta,medicamento; //variables de tratamiento
+if(aux2==1)
+{
+system("clear");
+string duracion,doctorquereceta,medicamento; //variables de tratamiento
 int cantidaddiaria;
 cout<<"Introduce el nombre del medicamento"<<endl;
 cin>>medicamento;
@@ -174,35 +206,41 @@ cout<<"Introduce la cantidad diaria"<<endl;
 cin>>cantidaddiaria;
 cout<<"Introduce el doctor que la receta"<<endl;
 cin>>doctorquereceta;
+	
 	Tratamiento tratamiento2(aux6,cantidaddiaria,duracion,doctorquereceta,medicamento);  //creamos el tratamiento
 	aux3.addTratamiento(tratamiento2);
 	constante.addTratamientohistorial(tratamiento2);  //al historial
 	system("clear");
 	cout<<"Tratamiento anadido con exito\n"<<endl;
+	
 cin.ignore();
 cin.get();
 }
-if(aux2==4){
-	system("clear");
+if(aux2==4)
+{
+system("clear");
 aux3.mostrarcitasdepaciente(aux6); //mostrar las citas del paciente con dni aux6
 cin.ignore();
 cin.get();
 }
-if(aux2==5){
-	system("clear");
-aux3.mostrartratamientosdepaciente(aux6);  // tratamientos
+	
+if(aux2==5)
+{
+system("clear");
+aux3.mostrartratamientosdepaciente(aux6);  //muestro tratamientos
 cin.ignore();
 cin.get();
 }
-if(aux2==3){
+if(aux2==3)
+{
 	system("clear");
 	cout<<"\n Citas registradas en historial: \n"<<endl;
-	constante.mostrarcitashistorial(aux6);    
+	constante.mostrarcitashistorial(aux6);           
 	cout<<"\n \n \n Tratamientos registrados en historial: \n"<<endl;
-	constante.mostrartratamientoshistorial(aux6);        //Muestro el historial 
+	constante.mostrartratamientoshistorial(aux6);                      //Muestro el historial 
 	cout<<"\n\n\n"<<endl;
 	cin.ignore();
-cin.get();
+	cin.get();
 }
 if(aux2==0)
 { system("clear");
@@ -225,12 +263,12 @@ cin>>elegir;
 			getline(cin,apellidos11);
 			cout<<"Introduce la fecha de nacimiento"<<endl;
 			cin>>fech1;
-			cout<<"Introduce el codigo postal"<<endl;   //Actualizo los datos
+			cout<<"Introduce el codigo postal"<<endl;      //Actualizo los datos
 			cin>>cp1;
 			cout<<"Introduce el telefono"<<endl;
 			cin>>telf1;
 			aux51=DNI1.substr(3,2);
-			idhistorial1=aux51;  //un simple id
+			idhistorial1=aux51;  //id
 			if(1!=aux3.anadepaciente2(nombre11,apellidos11,DNI1,idhistorial1,fech1,cp1,telf1)){cout<<"error"<<endl;}
 			else cout<<"Paciente modificado con exito"<<endl;
 			cin.ignore();
@@ -240,7 +278,7 @@ cin>>elegir;
 			{ system("clear");
 				string cod;
 						cout<<"Introduce el codigo de la cita que quieres borrar\n"<<endl;
-						cin>>cod;  //borro mi cita
+						cin>>cod;              //borro mi cita
 						 aux3.borracita(cod);
 						 cout<<"Borrado con exito\n"<<endl;
 						 cin.ignore();
@@ -254,7 +292,7 @@ cin>>elegir;
 				system("clear");
 				string medicam;
 				cout<<"Indica el medicamento del Tratamiento a borrar"<<endl;
-				cin>>medicam;            //Si es 3.- Borro mi tratamiento
+				cin>>medicam;                            //Si es 3.- Borro mi tratamiento
 				aux3.borratratamiento(aux6,medicam);
 				cout<<"\n Borrado con exito"<<endl;
 				cin.ignore();
@@ -268,14 +306,16 @@ cin>>elegir;
 
 
 }
-            					} else { cout<<"Paciente not found\n\n"<<endl; cin.ignore();
+            					} else { cout<<"Paciente not found\n\n"<<endl; cin.ignore(); //Esto es de la opcion 4
 										cin.get();}
-}
-}
+}//Fin de bucle de menu paciente
+	
+}//Fin de bucle de menu principal
 
-constante.escribehistorialTratamientos();
+constante.escribehistorialTratamientos();       //Guardo historial
 constante.escribehistorialCitas();
-aux3.escribePacientes(); //Guardo cuando cierro el programa todos los datos
+	
+aux3.escribePacientes();         //Guardo cuando cierro el programa todos los datos
 aux3.escribeCitas();
 aux3.escribeTratamientos();
 
